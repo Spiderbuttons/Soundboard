febb4aafecd27e5f5ef7045ab09b966668f6e7f7
--unset <--unset>
2025-06-10 14:59:32 -0400

    Initial commit.

---
+++ Soundboard/Helpers/Log.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using StardewModdingAPI;
+using System.Linq;
+using HarmonyLib;
+
+// ReSharper disable PossibleMultipleEnumeration
+
+namespace Soundboard.Helpers;
+
+public static class Log
+{
+    [Conditional("DEBUG")]
+    public static void Debug<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
+        LogLevel.Debug);
+
+    public static void Error<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
+        LogLevel.Error);
+
+    public static void Warn<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
+        LogLevel.Warn);
+
+    public static void Info<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
+        LogLevel.Info);
+
+    public static void Trace<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
+        LogLevel.Trace);
+
+    public static void Alert<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
+        LogLevel.Alert);
+
+    [Conditional("DEBUG")]
+    public static void ILCode(IEnumerable<CodeInstruction> newCode, IEnumerable<CodeInstruction> originalCode)
+    {
+        var originalEnumerator = 0;
+        foreach (var instruction in newCode)
+        {
+            if (originalEnumerator >= originalCode.Count())
+            {
+                Warn($"{instruction.opcode} {instruction.operand}");
+                continue;
+            }
+
+            if (instruction.opcode != originalCode.ElementAt(originalEnumerator).opcode ||
+                instruction.operand != originalCode.ElementAt(originalEnumerator).operand)
+            {
+                Warn($"{instruction.opcode} {instruction.operand}");
+                continue;
+            }
+
+            Debug($"{instruction.opcode} {instruction.operand}");
+            originalEnumerator++;
+        }
+    }
+
+    [Conditional("DEBUG")]
+    public static void ILCode(CodeInstruction code)
+    {
+        Debug($"{code.opcode} {code.operand}");
+    }
+}
---
+++ Soundboard/ModEntry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using Soundboard.Helpers;
+
+namespace Soundboard
+{
+    internal sealed class ModEntry : Mod
+    {
+        internal static IModHelper ModHelper { get; set; } = null!;
+        internal static IMonitor ModMonitor { get; set; } = null!;
+        internal static Harmony Harmony { get; set; } = null!;
+
+        public override void Entry(IModHelper helper)
+        {
+            ModHelper = helper;
+            ModMonitor = Monitor;
+            Harmony = new Harmony(ModManifest.UniqueID);
+
+            Harmony.PatchAll();
+
+            Helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+        }
+    }
+}
